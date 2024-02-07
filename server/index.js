e32dbbd7cc1bbb2de53e300f24d139fe213716e1
kanishka0007 <90264092+kanishka0007@users.noreply.github.com>
2024-02-07 17:22:56 +0530

    update
    changes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 // const express = require("express")
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import Connection from './database/db.js';
+
 const app = express();
 
 dotenv.config();
@@ -11,3 +14,8 @@ const USERNAME=process.env.DB_USERNAME;
 const PASSWORD=process.env.DB_PASSWORD;
 Connection(USERNAME, PASSWORD);
 app.listen(PORT,()=> console.log(`Server is runnning on PORT ${PORT}`));
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/', Routes);

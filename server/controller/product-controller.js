f9619092d81a7418b268165ca50f383d6b089cde
Rakhi088 <130650943+Rakhi088@users.noreply.github.com>
2024-02-08 08:19:14 +0530

    update
    changes

--- server/controller/product-controller.js
+++ server/controller/product-controller.js
@@ -15,7 +15,11 @@ export const getProductById = async (request, response) => {
     try {
         const products = await Product.findOne({ 'id': request.params.id });
         response.json(products);
-    }catch (error) {
 
+        const id = request.params.id;
+        const product = await Product.findOne({'id': id})
+        response.status(200).json(product);
+    }catch (error) {
+        response.status(500).json({message: error.message});
     }
 }
